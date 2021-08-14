f3bd78dbe06ef6802f9d94d3533775b61f2ef144
min92mon <min92mon@gmail.com>
2021-08-14 13:58:03 +0900

    Initial Commit

---
+++ ChatBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatBoard
+//
+//  Created by 정민영 on 2021/08/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
