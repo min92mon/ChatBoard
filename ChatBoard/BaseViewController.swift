6c6e3ad96737eb3dc43cd1f973c48205b4d5076b
min92mon <min92mon@gmail.com>
2021-08-19 23:16:19 +0900

    Add Alert Function in BaseViewController

--- ChatBoard/BaseViewController.swift
+++ ChatBoard/BaseViewController.swift
@@ -17,3 +17,17 @@ class BaseViewController : UIViewController {
     }
 }
 
+// MARK: - Setup Simple Alert
+extension BaseViewController {
+    func alert(_ message: String, completion: (()->Void)? = nil) {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "확인", style: .cancel) { (_) in
+                completion?()
+            }
+            alert.addAction(okAction)
+            self.present(alert, animated: false)
+        }
+    }
+}
